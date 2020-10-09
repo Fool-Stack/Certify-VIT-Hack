c3db273c0a3241b287bfea08e18c287106579d22
jugalbhatt123 <jugalbhatt3@gmail.com>
2020-10-09 15:35:21 +0530

    auth middleware

---
+++ Backend/middleware/checkAuthAdmin.js
@@ -0,0 +1,20 @@
+const JWT = require("jsonwebtoken");
+
+module.exports = function (req, res, next) {
+  const token = req.header("auth-token");
+  if (!token) return res.status(400).send("Access Denied!, no token entered");
+
+  try {
+    const verified = JWT.verify(token, process.env.jwtSecret);
+    req.user = verified;
+    if (req.user.userType === "Admin") {
+      next();
+    } else {
+      res.status(409).json({
+        message: "not an Admin",
+      });
+    }
+  } catch (err) {
+    res.status(400).send({ error: "auth failed, check auth-token" });
+  }
+};
