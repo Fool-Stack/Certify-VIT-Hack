2194517513fbc4b1b369507a7cc4781fef55556e
Sarthak <sarthakbh321@gmail.com>
2020-10-09 23:02:32 +0530

    added drawer

--- Frontend/src/components/DashNavbar/DashNavbar.js
+++ Frontend/src/components/DashNavbar/DashNavbar.js
@@ -1,18 +1,22 @@
 //Copy of Navbar.js
 
 import React from "react";
-import { AppBar, Avatar, Toolbar } from "@material-ui/core";
+import { AppBar, Avatar, IconButton, Toolbar } from "@material-ui/core";
 import "./DashNavbar.css";
+import { Menu } from "@material-ui/icons";
 
-function DashNavbar() {
+function DashNavbar({ open, setOpen }) {
 	return (
 		<AppBar position="static" className="navbar" elevation={0}>
 			<Toolbar className="nav-toolbar">
+				<IconButton edge="start" onClick={() => setOpen(!open)}>
+					<Menu className="nav-drawer-icon" fontSize="large" />
+				</IconButton>
 				<img
 					src="./assets/certify.svg"
 					alt="brand logo"
 					width={150}
-					className="nav-logo"
+					className="nav-logo m-right"
 				/>
 				<div className="nav-menu nav-name-tag">
 					Welcome Sarthak
--- Frontend/src/pages/Dashboard/Dashboard.js
+++ Frontend/src/pages/Dashboard/Dashboard.js
@@ -1,11 +1,30 @@
-import React from "react";
+import { Drawer } from "@material-ui/core";
+import React, { useState } from "react";
 import DashNavbar from "../../components/DashNavbar/DashNavbar";
+import "./Dashboard.css";
 
 function Dashboard() {
+	const [drawerOpen, setDrawerOpen] = useState(true);
+	const drawerWidth = 200;
+
 	return (
-		<div className="dashboard-page">
-			<DashNavbar />
-			<h1>Dashboard</h1>
+		<div
+			className="dashboard-page"
+			style={{ marginLeft: drawerOpen ? `${drawerWidth}px` : 0 }}
+		>
+			<DashNavbar open={drawerOpen} setOpen={setDrawerOpen} />
+			<Drawer
+				variant="persistent"
+				anchor="left"
+				open={drawerOpen}
+				className="dash-drawer"
+				style={{ width: `${drawerWidth}px` }}
+			>
+				<h1>Hello</h1>
+			</Drawer>
+			<div className="dash-screen">
+				<h1>Dashboard</h1>
+			</div>
 		</div>
 	);
 }
