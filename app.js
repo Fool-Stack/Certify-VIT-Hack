e64306ecabbdb488bf269fc6e9c426afca18bb1a
jugalbhatt123 <jugalbhatt3@gmail.com>
2020-10-10 14:15:11 +0530

    cors trying

--- app.js
+++ app.js
@@ -81,6 +81,10 @@ app.use((req, res, next) => {
 	next(error);
 });
 
+app.get('/test', (req, res)=>{
+  res.send("Hello")
+})
+
 app.use((error, req, res, next) => {
 	res.status(error.status || 500);
 	res.json({
