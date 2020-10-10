ceae3b7cda1a8088d1d5cc88e9a13f00c8f93d44
jugalbhatt123 <jugalbhatt3@gmail.com>
2020-10-10 14:14:18 +0530

    cors trying

--- app.js
+++ app.js
@@ -58,20 +58,20 @@ app.use('/certificate', certificateRoutes)
 app.use(limiter);
 
 // Allow CORS
-app.use((req, res, next) => {
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header(
-		"Access-Control-Allow-Headers",
-		"Origin, X-Requested-With, Content-Type, Accept, Authorization,auth-token"
-	);
-	if (req.method === "OPTIONS") {
-		res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
-		return res.status(200).json({});
-	}
-	next();
-});
+// app.use((req, res, next) => {
+// 	res.header("Access-Control-Allow-Origin", "*");
+// 	res.header(
+// 		"Access-Control-Allow-Headers",
+// 		"Origin, X-Requested-With, Content-Type, Accept, Authorization,auth-token"
+// 	);
+// 	if (req.method === "OPTIONS") {
+// 		res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+// 		return res.status(200).json({});
+// 	}
+// 	next();
+// });
 
-//app.use(cors());
+app.use(cors());
 
 
 //route not found
