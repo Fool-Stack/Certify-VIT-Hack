a679c6606c7a123df933ac77c00e107de08eb55c
Sarthak <sarthakbh321@gmail.com>
2020-10-10 19:23:28 +0530

    started my certificates section

--- Frontend/src/pages/Dashboard/Dashboard.js
+++ Frontend/src/pages/Dashboard/Dashboard.js
@@ -4,6 +4,7 @@ import { Redirect } from "react-router-dom";
 import DashNavbar from "../../components/DashNavbar/DashNavbar";
 import Sidebar from "../../components/Sidebar/Sidebar";
 import Loading from "../Loading/Loading";
+import MyCertificates from "../MyCertificates/MyCertificates";
 import "./Dashboard.css";
 
 function Dashboard() {
@@ -11,16 +12,25 @@ function Dashboard() {
 	const [loading, setLoading] = useState(true);
 
 	const [name, setName] = useState("");
+	const [certificates, setCertificates] = useState([]);
 
 	const [openDash, setOpenDash] = useState(1);
 
 	const [drawerOpen, setDrawerOpen] = useState(false);
 	const drawerWidth = 256;
 
+	const getCertificates = async () => {};
+
+	const dashboardSection = () => {
+		if (openDash === 1) return <MyCertificates certs={certificates} />;
+		else if (openDash === 2) return <MyEvents />;
+	};
+
 	useEffect(() => {
 		if (localStorage.getItem("authToken")) {
 			setLoggedIn(true);
 			setName(localStorage.getItem("name"));
+			getCertificates();
 		} else setLoggedIn(false);
 		setLoading(false);
 	}, []);
@@ -53,7 +63,8 @@ function Dashboard() {
 				/>
 			</Drawer>
 			<div className="dash-screen">
-				<h1>Dashboard {openDash}</h1>
+				{/* <h1>Dashboard {openDash}</h1> */}
+				<MyCertificates certs={certificates} />
 			</div>
 		</div>
 	);
---
+++ Frontend/src/pages/MyCertificates/MyCertificates.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./MyCertificates.css";
+
+function MyCertificates({ certs }) {
+	return (
+		<div className="certificates-section">
+			<h1 className="section-heading">
+				<span style={{ borderBottom: "3px white solid" }}>
+					MY CERTIFICATES
+				</span>
+			</h1>
+			<div className="certificate-list">
+				{certs.length == 0 ? (
+					<h3 className="no-cert">
+						You do not have any certificates right now!
+					</h3>
+				) : null}
+			</div>
+		</div>
+	);
+}
+
+export default MyCertificates;
